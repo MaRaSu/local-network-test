e267bd08c6ecce27775c28011575a0f6227fa7e3
MaRaSu <markus.r.suomi@gmail.com>
2018-02-16 21:31:10 +0200

    Fixed arp cmd differences

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 var find = require('local-devices');
 var WATCH_LIST = [
   ["0:b3:62:71:c4:77" , "Nella"],
+  ["00:b3:62:71:c4:77" , "Nella"],
   ["30:63:6b:d5:0:51", "Markus"],
+  ["30:63:6b:d5:00:51", "Markus"],
   ["e4:e4:ab:20:e9:bf", "Milla"],
   ["4c:7c:5f:76:0e:a1", "Kiti"]
 ]; 
 
 async function skannaa() {
+  console.log("Etsitään");
   let a = await find(); 
-  console.log("Etsitään"); 
+
   return a;
 }
 
