d5450d2b445e4d1d687e00aa0ad4f75c0345ff04
MaRaSu <markus.r.suomi@gmail.com>
2018-02-16 21:18:56 +0200

    Search for family members

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 //import find from 'local-devices';
 var find = require('local-devices');
-// var devicesList;
+var WATCH_LIST = [
+  ["0:b3:62:71:c4:77" , "Nella"],
+  ["30:63:6b:d5:0:51", "Markus"],
+  ["e4:e4:ab:20:e9:bf", "Milla"],
+  ["4c:7c:5f:76:0e:a1", "Kiti"]
+]; 
 
 async function skannaa() {
   let a = await find(); 
-  console.log("Hop"); 
+  console.log("Etsitään"); 
   return a;
 }
 
 skannaa().then(printtaa);
 
 function printtaa(devices) {
+  let deviceCount = 0;
   for(let i in devices) {
     if(devices[i].mac !== "<incomplete>") {
-      console.log(devices[i]);
+      for(let b in WATCH_LIST) {
+        if(devices[i].mac.includes(WATCH_LIST[b][0])) {
+          console.log("Löytyi: " + WATCH_LIST[b][1]);
+        }
+      }
     }
+    deviceCount = i;
   }
+  console.log("Löydetty " + deviceCount + " kpl laitteita");
+
 }
